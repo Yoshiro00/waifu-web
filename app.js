ae748b4c050a1c5e897584e41572dc61913a3385
Yoshiro00 <98204934+Yoshiro00@users.noreply.github.com>
2023-02-16 19:35:38 +0800

    Upgrade

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-let var1 = document.getElementById('')
+
+let var1 = ["Hello, Hello", "Hello, Hi", "Hello, Hallo"];
+let var2 = 0;
+
+window.addEventListener('load', function() {
+    let button = document.querySelector('#appbuttonid');
+    let header1 = document.getElementById('appheaderid');
+    let header2 = document.getElementById('appnumid');
+
+    button.addEventListener('click', function() {
+        var2++;
+        header2.textContent = var2++;
+    });
+
+    switch(var2){
+        default:
+        case var2 = 0:
+            document.getElementById('appheaderid').innerText = var2[0];
+            break;
+        case var2 > 10:
+            document.getElementById('appheaderid').innerText = var2[1];
+            break;
+        case var2 > 20:
+            document.getElementById('appheaderid').innerText = var2[2];
+            break;
+    }
+});
