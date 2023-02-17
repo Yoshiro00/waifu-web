ea30c8c8d14e745e73f89f546b63fb6d8b73654c
Yoshiro00 <98204934+Yoshiro00@users.noreply.github.com>
2023-02-17 15:33:33 +0800

    New Web App

--- app.js
+++ app.js
@@ -1,27 +1,38 @@
-
+let header1 = document.getElementById('appheaderid');
 let var1 = ["Hello, Hello", "Hello, Hi", "Hello, Hallo"];
 let var2 = 0;
 
 window.addEventListener('load', function() {
     let button = document.querySelector('#appbuttonid');
-    let header1 = document.getElementById('appheaderid');
     let header2 = document.getElementById('appnumid');
 
     button.addEventListener('click', function() {
         var2++;
         header2.textContent = var2++;
     });
-
-    switch(var2){
-        default:
-        case var2 = 0:
-            header1.textContent = "Hello";
-            break;
-        case var2 = 10:
-            header1.textContent = "Im Under The Water";
-            break;
-        case var2 = 20:
-            header1.textContent = "Please Help me";
-            break;
-    }
 });
+
+function MiniTextBot() {
+    let inputBox = document.getElementById("inputText").value;
+    let buttonShow = document.querySelector('#linkid1');
+    let text;
+
+    switch(inputBox){
+        case inputBox = "Hello":
+            text = "Hi";
+            break;
+        case inputBox = "What is your name?":
+            text = "My Name is.....";
+            break;
+        case inputBox = "How are you?":
+            text = "I'm good, how about you? :)";
+            break;
+        case inputBox = "01195":
+            text = "tap this link";
+            buttonShow.style.left = "30%";
+            break;
+        default:
+            text = "i don't know that word yet or maybe your missing something";
+    }
+    document.getElementById("appheaderid").innerHTML = text;
+}
